9dd6af539a2b3e090ad4a6d1b3734d6f6af781af
BimmerBass <abildskovniels@gmail.com>
2021-04-08 17:03:25 +0200

    Added promotion and en-passant scoring:
    Score of Loki_dev vs baseline: 134 - 121 - 145 [0.516]
    ...      Loki_dev playing White: 70 - 55 - 75  [0.537] 200
    ...      Loki_dev playing Black: 64 - 66 - 70  [0.495] 200
    ...      White vs Black: 136 - 119 - 145  [0.521] 400
    Elo difference: 11.3 +/- 27.2, LOS: 79.2 %, DrawRatio: 36.3 %
    400 of 400 games finished.
    
    Current elo estimate: 2418

--- Loki/thread.cpp
+++ Loki/thread.cpp
@@ -66,21 +66,21 @@ void SearchThread_t::score_moves(MoveList* ml) {
 				(*ml)[i]->score += MvvLva[pos->piece_list[pos->side_to_move][FROMSQ((*ml)[i]->move)]][pos->piece_list[Them][TOSQ((*ml)[i]->move)]];
 			}
 			
-			// Promotions will also be scored highly.
-			//if (SPECIAL((*ml)[i]->move) == PROMOTION) {
-			//	// If the promotion piece is a queen or we're capturing a piece, we'll score this just below the hash move
-			//	if (PROMTO((*ml)[i]->move) + 1 == QUEEN) {
-			//		(*ml)[i]->score = hash_move_sort - 100;
-			//	}
-			//	else { // Will be scored as loosing a pawn and gaining the promotion piece.
-			//		(*ml)[i]->score += MvvLva[PAWN][PROMTO((*ml)[i]->move) + 1];
-			//	}
-			//}
-			//
-			//// En-passants will be scored simply as PxP
-			//if (SPECIAL((*ml)[i]->move) == ENPASSANT) {
-			//	(*ml)[i]->score += MvvLva[PAWN][PAWN];
-			//}
+			// Promotion and en-passant scoring (~11 elo)
+			if (SPECIAL((*ml)[i]->move) == PROMOTION) {
+				// If the promotion piece is a queen or we're capturing a piece, we'll score this just below the hash move
+				if (PROMTO((*ml)[i]->move) + 1 == QUEEN) {
+					(*ml)[i]->score = hash_move_sort - 100;
+				}
+				else { // Will be scored as loosing a pawn and gaining the promotion piece.
+					(*ml)[i]->score += MvvLva[PAWN][PROMTO((*ml)[i]->move) + 1];
+				}
+			}
+			
+			// En-passants will be scored simply as PxP
+			if (SPECIAL((*ml)[i]->move) == ENPASSANT) {
+				(*ml)[i]->score += MvvLva[PAWN][PAWN];
+			}
 		
 		}
 	
