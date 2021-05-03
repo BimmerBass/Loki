ca29b8231f447764df98550156902c7090d8714b
BimmerBass <abildskovniels@gmail.com>
2021-05-03 15:28:11 +0200

    Tested new base reductions in LMR. The current one didn't really change performance. Will try a linear/logarithmic one now

--- Loki/search.cpp
+++ Loki/search.cpp
@@ -869,7 +869,8 @@ namespace Search {
 				// Step 13A. Late move reductions (~92 elo). If we haven't raised alpha yet, we're probably in an ALL-node,
 				//	so we'll reduce the search depth and do a full-depth re-search if the score is (surprisingly) above alpha.
 				if (moves_searched >= lmr_limit && depth > lmr_depth && !is_tactical && !is_pv && !root_node && extensions == 0) {
-					int R = 1; // For now, we use an initial reduction of one ply only.
+					//int R = 1; // For now, we use an initial reduction of one ply only.
+					int R = late_move_reduction(depth, moves_searched);
 
 					// Increase reduction for moves with history < 0 (~5 elo)
 					if (ss->stats.history[(ss->pos->side_to_move == WHITE) ? BLACK : WHITE][fromSq][toSq] < 0) {
@@ -1244,23 +1245,23 @@ void Search::INIT() {
 	}
 
 	// Initialize table of late move reductions
-	for (int d = 1; d < MAXDEPTH; d++) {
+	for (double d = 1; d < MAXDEPTH; d += 1.0) {
 
-		for (int c = 1; c < MAXPOSITIONMOVES; c++) {
+		for (double c = 1; c < MAXPOSITIONMOVES; c += 1.0) {
 			//Reductions[d][c] = (int)std::round(0.75 + (std::log(2.0 * double(d)) * std::log(2.0 * double(c))) / 2.75);
 			//Reductions[d][c] = (int)std::round((std::log(2.0 * double(d)) * std::log(2.0 * double(c))) / 2.75);
 			//Reductions[d][c] = (int)std::round(0.75 + (std::log(d) * std::log(c)) / 2.75);
-			Reductions[d][c] = (int)std::round((std::log(d) * std::log(c)) / 2.75);
+			//Reductions[d][c] = (int)std::round((std::log(d) * std::log(c)) / 2.75);
 			//Reductions[d][c] = (int)(std::log(2.0 * double(d) * std::log(2.0 * double(c)))) / 3.0;
 			//Reductions[d][c] = (int)std::round((std::log(double(d)) * std::log(double(c))) / 2.75);
+			Reductions[(int)d][(int)c] = (int)std::round((std::log(3.0 * d) * std::log(3.0 * c)) / 7.0);
 
-			if (Reductions[d][c] < 1) {
-				Reductions[d][c] = 1;
+			if (Reductions[(int)d][(int)c] < 1) {
+				Reductions[(int)d][(int)c] = 1;
 			}
+
 		}
 	}
-
-
 
 	// Initialize null move R-value table
 	for (int d = 0; d < MAXDEPTH; d++) {
