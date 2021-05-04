9494428f4966c7429ec1de72dd9ca815084665b6
BimmerBass <abildskovniels@gmail.com>
2021-05-04 15:23:16 +0200

    Added command line argument parsing to run bench directly.
    Bench: 12743158

--- Loki/main.cpp
+++ Loki/main.cpp
@@ -2,13 +2,19 @@
 #include "texel.h"
 
 
-int main() {
+int main(int argc, char* argv[]) {
 	BBS::INIT();
 	Magics::INIT();
 	Search::INIT();
 	Eval::INIT();
 	PSQT::INIT();
 
+
+	// If "bench" has been added as an argument, just run this and quit
+	if (argc > 1 && !strncmp(argv[1], "bench", 5)) {
+		Bench::run_benchmark();
+		return 0;
+	}
 
 	UCI::loop();
 
