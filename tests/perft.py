776faad74d06f3fa58d030c511b83410d3631b08
BimmerBass <abildskovniels@gmail.com>
2021-05-07 15:47:33 +0200

    Fixed the perft script. It should work properly now.

--- tests/perft.py
+++ tests/perft.py
@@ -5,6 +5,7 @@ This python script will run through a list of FEN's with perft count given for e
 '''
 import argparse
 import subprocess
+import sys
 
 # Class for holding a position and its perft results
 class PerftPosition:
@@ -34,19 +35,22 @@ def main():
 
     # Step 2. Run the engine
     prc = subprocess.Popen(args.exe, 
-                    stderr=subprocess.STDOUT,
+                    stderr=subprocess.PIPE,
                     stdout=subprocess.PIPE,
                     stdin=subprocess.PIPE,
                     universal_newlines=True)
 
     # Step 3. Read the epd file
     Positions = []
+    EPDS = []
 
     with open(args.epd, "r") as perft_file:
         Lines = perft_file.readlines()
 
         # Step 3A. Read through the EPD line-by-line
         for line in Lines:
+            EPDS.append(line)
+
             elements = line.split(";")
 
             FEN = elements[0]
@@ -54,36 +58,63 @@ def main():
 
             for i in elements[1:]:
                 nodesByDepth.append(int(i[2:]))
-            
-            # Step 3A.1. Append the FEN and depths
-            Positions.append(PerftPosition(FEN, len(nodesByDepth), nodesByDepth))
+
+            # Step 3A.1. Find the smallest depth that has a node-count for the position
+            smallest_depth = int(elements[1].split(" ")[0][-1])
+
+            # Step 3A.2. Append the FEN and depths
+            PositionData = (FEN, len(nodesByDepth), nodesByDepth, smallest_depth)
+            Positions.append(PositionData)
 
 
 
     # Step 4. Run through the positions and call perft for each depth
     n = 0
 
+    # Step 4A. When starting up, Loki outputs some info on the transposition table,
+    found = prc.stdout.readline().strip()
+    prc.stdout.flush()
+    found = prc.stdout.readline().strip()
+    prc.stdout.flush()
+
     for i in Positions:
-        Fen = i.fen
+        if (i[3] >= args.depth):
+            continue
+
+        Fen = i[0]
         n += 1
         print("[*] Now perft-testing position nr. " + str(n))
         
         # Step 4A. Send the position to the engine.
         prc.stdin.write('position fen %s \n' % (Fen))
 
-        for d in range(i.depths):
-            if (d >= args.depth):
+
+        for d in range(0, i[1]):
+            if d == 0:
+                continue
+            if d >= args.depth or d < i[3]:
                 break
 
             prc.stdin.write('perft depth %d \n' % (d + 1))
             prc.stdin.flush()
 
-            found = int(prc.stdout.readline().strip())
+            #found = prc.stdout.readline().strip()
+            #print(found)
+            found = []
 
-            if found == i.nodeCounts[d]:
-                print("Depth " + str(d + 1) + ": PASSED")
+            while True:
+                found.append(prc.stdout.readline().strip())
+                prc.stdout.flush()
+                if "Nodes visited" in found[-1]:
+                    break
+            
+            nodecount = int(found[-1].split(": ")[-1])
+
+            if nodecount == i[2][d]:
+                print("Depth " + str(d) + ": PASSED")
             else:
-                print("Depth " + str(d + 1) + ": FAILED ----> count: " + str(found))
+                print("Depth " + str(d) + ": FAILED ----> count: " + str(nodecount))
+
  
     # Step 5. Close Loki
     prc.stdin.write('quit')
