c815eaafa32d0c73db9781861c107cf27e24fb9a
BimmerBass <abildskovniels@gmail.com>
2021-05-09 20:25:13 +0200

    Tested LMR against master and got really good results:
    ELO   | 110.75 +- 26.26 (95%)
    SPRT  | 10.0+0.1s Threads=1 Hash=16MB
    LLR   | 3.09 (-2.94, 2.94) [0.00, 5.00]
    Games | N: 548 W: 285 L: 116 D: 147
    
    Bench: 16503484

--- Loki/search.cpp
+++ Loki/search.cpp
@@ -865,7 +865,7 @@ namespace Search {
 
 			else {
 				
-				// Step 13A. Late move reductions (~115 elo). If we haven't raised alpha yet, we're probably in an ALL-node,
+				// Step 13A. Late move reductions (~111 elo). If we haven't raised alpha yet, we're probably in an ALL-node,
 				//	so we'll reduce the search depth and do a full-depth re-search if the score is (surprisingly) above alpha.
 				// NOTE: All elo measurements below are from self-play. They are used to give an image of which features give the best returns, but they
 				//	shouldn't be taken literally. Their sum is bigger than the real elo gain of LMR.
