6e4fbebbc1106f2ee03f7a5acd45982d112a06e6
BimmerBass <abildskovniels@gmail.com>
2021-05-16 12:31:35 +0200

    Removed everything. Will start again now.

--- Loki/main.cpp
+++ Loki/main.cpp
@@ -20,22 +20,7 @@ int main(int argc, char* argv[]) {
 
 	//std::vector<int> arch = { 256, 32, 32 };
 
-	std::vector<int> arch = { 256 };
 
-	Neural::NeuralNet network(arch);
-
-	network.train_model(30000);
-
-	//Texel::Parameters tuning_variables;
-	//
-	//using namespace Texel;
-	//
-	//tuning_variables.push_back(Parameter(&bishop_pair, Value(0.0015, 0.0015), Value(3.0, 3.0)));
-	//tuning_variables.push_back(Parameter(&knight_pawn_penaly, Value(0.001, 0.001), Value(2.0, 2.0), Score(INF, INF), Score(1, 1)));
-	//tuning_variables.push_back(Parameter(&rook_pawn_bonus, Value(0.001, 0.001), Value(2.0, 2.0), Score(INF, INF), Score(1, 1)));
-	//
-	//
-	//Texel::Tune(tuning_variables, "C:\\Users\\abild\\Desktop\\quiet-labeled.epd", 1000);
 
 	return 0;
 }
