af77720d2fe29a9e38748df0ba62517817efe391
Niels Abildskov <49415233+BimmerBass@users.noreply.github.com>
2021-06-02 21:09:00 +0200

    Began adding new data generation logic.

--- Loki/data/generate.cpp
+++ Loki/data/generate.cpp
@@ -2,5 +2,30 @@
 
 
 namespace DataGeneration {
+	
+	// Setup of a saved position
+	void SavedBoard::setup(const GameState_t* pos, int evaluation) {
+		// Step 1. Copy the gamestate.
+		pieces[PAWN][WHITE] = pos->pieceBBS[PAWN][WHITE];
+		pieces[KNIGHT][WHITE] = pos->pieceBBS[KNIGHT][WHITE];
+		pieces[BISHOP][WHITE] = pos->pieceBBS[BISHOP][WHITE];
+		pieces[ROOK][WHITE] = pos->pieceBBS[ROOK][WHITE];
+		pieces[QUEEN][WHITE] = pos->pieceBBS[QUEEN][WHITE];
+		pieces[KING][WHITE] = pos->pieceBBS[KING][WHITE];
 
+		pieces[PAWN][BLACK] = pos->pieceBBS[PAWN][BLACK];
+		pieces[KNIGHT][BLACK] = pos->pieceBBS[KNIGHT][BLACK];
+		pieces[BISHOP][BLACK] = pos->pieceBBS[BISHOP][BLACK];
+		pieces[ROOK][BLACK] = pos->pieceBBS[ROOK][BLACK];
+		pieces[QUEEN][BLACK] = pos->pieceBBS[QUEEN][BLACK];
+		pieces[KING][BLACK] = pos->pieceBBS[KING][BLACK];
+
+		stm = pos->side_to_move;
+		en_passant = pos->enPasSq;
+		castling_rights = pos->castleRights;
+		move50 = pos->fiftyMove;
+
+		// Step 2. Save the score
+		score = evaluation;
+	}
 }
--- Loki/data/generate.h
+++ Loki/data/generate.h
@@ -15,7 +15,48 @@ namespace DataGeneration {
     constexpr size_t MAX_BATCH_SIZE = 5000000;
     static_assert(MAX_BATCH_SIZE > 0);
 
+    
+    // This struct holds all data that represents one position.
+    struct SavedBoard {
+        // Board state
+        Bitboard pieces[6][2] = { {0} };
+        SIDE stm = WHITE;
+        int en_passant = NO_SQ;
+        int castling_rights = 0;
+        int move50 = 0;
 
+        // Score
+        int score = 0;
+
+        void setup(const GameState_t* pos, int evaluation);
+    };
+
+
+
+    // This class is responsible for playing a predetermined number of games on one thread.
+    // Each thread will get their own
+    class Arbiter {
+    public:
+        Arbiter(size_t _count, SearchInfo_t* _settings, bool play_random = false);
+        ~Arbiter();
+
+        // Runs the specified number of games.
+        void run();
+
+    private:
+        // Used for random position generation.
+        const bool random_play;
+
+        GameState_t* board;
+        SearchThread_t* searcher;
+
+        // Play one game.
+        void play_game();
+
+        // Holds all positions that we have reached.
+        // Note: Ealy opening (<= move 3) and checkmate/stalemate positions are left out.
+        std::vector<SavedBoard> positions;
+    };
 }
 
 
