269e11bf5135d1a1832d3ea4ee825c00f914f6ec
Niels Abildskov <49415233+BimmerBass@users.noreply.github.com>
2021-06-18 18:00:59 +0200

    Remove unused variables

--- Loki/lnn/types/architecture.h
+++ Loki/lnn/types/architecture.h
@@ -11,9 +11,7 @@ typedef float neuron_t;
 constexpr size_t INPUT_SIZE = 768; // Twelve pieces with 64 squares each
 constexpr size_t FIRST_HIDDEN_SIZE = 256;
 constexpr size_t HIDDEN_STD_SIZE = 32;
-constexpr int HIDDEN_STD_COUNT = 2;
 constexpr size_t OUTPUT_SIZE = 1;
-constexpr size_t LAYER_COUNT = 5;
 
 
 #endif
