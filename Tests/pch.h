12f5cdf1fd17d9827ac9587e1f92119b791642c3
BimmerBass <abildskovniels@gmail.com>
2022-04-03 18:07:24 +0200

    Add license texts

--- Tests/pch.h
+++ Tests/pch.h
@@ -4,6 +4,24 @@
 // However, files listed here are ALL re-compiled if any one of them is updated between builds.
 // Do not add files here that you will be updating frequently as this negates the performance advantage.
 
+//
+//	Loki, a UCI-compliant chess playing software
+//	Copyright (C) 2021  Niels Abildskov (https://github.com/BimmerBass)
+//
+//	Loki is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	Loki is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
 #ifndef PCH_H
 #define PCH_H
 
